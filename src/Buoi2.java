84fc9ac2ce423eb377f62421ece41a2c7b76f5ad
khoa <hatgiong@gmail.com>
2021-10-24 14:14:16 +0700

    luu moi

---
+++ src/Buoi2.java
@@ -0,0 +1,18 @@
+public class Buoi2 {
+    public static void main(String[] args) {
+        String name="Phạm Đăng Khoa";
+        int age= 37;
+        String address="Hòa Thành, Tây Ninh";
+        boolean isMale=true;
+        String sothich="Ở không";
+        float height=1.65f;
+        System.out.println("Tên: "+ name);
+        System.out.println("Tuổi"+ age);
+        System.out.println("Sở thích"+ sothich);
+        System.out.println("Chiều cao:"+ height);
+        System.out.println("Giới tính:"+ isMale);
+
+
+    }
+
+}
